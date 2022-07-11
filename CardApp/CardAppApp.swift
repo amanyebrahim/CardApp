b01854e28c85ffcf130c767d6d52fe59b7eb7672
amanyEbrahim <amany.ebrahim@kuwaitnet.dev>
2022-07-11 23:09:42 +0200

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by amany on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
