483ef74b5b1d8e4f45457d40a164fd83b65d4bc8
amanyEbrahim <amany.ebrahim@kuwaitnet.dev>
2022-07-27 11:48:47 +0200

    add card app

--- CardApp/ContentView.swift
+++ CardApp/ContentView.swift
@@ -8,10 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card2"
+     @State var cpuCard = "card3"
+     @State var playerScore = 0
+     @State var cpuScore = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            
+            Image("background").ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+            
+                }
+                Spacer()
+                Button(action: {
+                    
+                    let playerRandom = Int.random(in:2...14)
+                    let cpuRandom = Int.random(in:2...14)
+                    
+                    playerCard = "card" + String(playerRandom)
+                    cpuCard = "card" + String(cpuRandom)
+                    
+                    if playerRandom > cpuRandom{
+                        playerScore += 1
+                    }else if cpuRandom > playerRandom {
+                        cpuScore += 1
+                    }
+                }, label:{
+                    Image("dealbutton")
+                    
+                })
+      
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 5.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("Cpu")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 5.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    
+                }
+                
+                Spacer()
+                
+                
+            }
+            
+            
+        }
+        
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
